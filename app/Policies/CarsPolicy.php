c1173a4fc700f00378c0a18fcb27018dd755dcd9
Izan <izgoar@fp.insjoaquimmir.cat>
2024-04-23 19:40:10 +0200

    policies vacias creadas

---
+++ app/Policies/CarsPolicy.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+
+class CarsPolicy
+{
+    /**
+     * Create a new policy instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
---
+++ app/Policies/ExhaustpipePolicy.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+
+class ExhaustpipePolicy
+{
+    /**
+     * Create a new policy instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
