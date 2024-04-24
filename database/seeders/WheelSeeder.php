0eb3b8c23135864edacd4dd69525ff85d049013b
Izan <izgoar@fp.insjoaquimmir.cat>
2024-04-24 20:07:42 +0200

    plantilla seeder

--- database/seeders/WheelSeeder.php
+++ database/seeders/WheelSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class WheelSeeder extends Seeder
 {
@@ -12,6 +13,25 @@ class WheelSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('wheels')->insert([
+            [
+                'name' => 'oz',
+                'type' => 'aluminio',
+                'inch' => '19',
+                'price' => '180',
+            ],
+            [
+                'name' => 'ronal',
+                'type' => 'aluminio',
+                'inch' => '18',
+                'price' => '150',
+            ],
+            [
+                'name' => 'japan racing',
+                'type' => 'aluminio',
+                'inch' => '19',
+                'price' => '200',
+            ]
+        ]);
     }
 }
