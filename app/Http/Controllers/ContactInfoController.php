c4ae9ddb402b2ba575c31328d9b64fd6a81c27f9
Izan <izgoar@fp.insjoaquimmir.cat>
2024-05-16 17:20:58 +0200

    contacto

--- app/Http/Controllers/ContactInfoController.php
+++ app/Http/Controllers/ContactInfoController.php
@@ -7,9 +7,6 @@ use App\Models\ContactInfo;
 
 class ContactInfoController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         $contact = ContactInfo::all();
@@ -19,9 +16,6 @@ class ContactInfoController extends Controller
         ], 200);
     }
 
-    // /**
-    //  * Show the form for creating a new resource.
-    //  */
     public function store(Request $request)
     {
         $request->validate([
@@ -40,9 +34,6 @@ class ContactInfoController extends Controller
         
     }
 
-    // /**
-    //  * Display the specified resource.
-    //  */
     public function show(ContactInfo $contact)
     {
         return response()->json([
@@ -51,9 +42,6 @@ class ContactInfoController extends Controller
         ], 200);
     }
 
-    // /**
-    //  * Show the form for editing the specified resource.
-    //  */
     public function update(Request $request, ContactInfo $contact)
     {
         $request->validate([
@@ -72,9 +60,6 @@ class ContactInfoController extends Controller
         
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(ContactInfo $contact)
     {
         $contact->delete();
